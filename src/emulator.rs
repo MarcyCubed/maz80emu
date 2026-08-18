232eb4b5e8253707f9ec3a1199b3c3513da9b7e3
Marcy Cubed <193825312+MarcyCubed@users.noreply.github.com>
2026-08-18 16:02:38 +0100

    Simple array-based memory support.

--- src/emulator.rs
+++ src/emulator.rs
@@ -40,4 +40,73 @@ impl Emulator {
             self.micros[0](&mut self.state)
         }
     }
+
+    /// Run the emulator with memory.
+    ///
+    /// Handle memory access for the emulator. If the requested address is out of bounds, the
+    /// load or store request will be returned as in [[Emulator::run]].
+    pub fn run_with_memory(&mut self, memory: &mut [u8]) -> ExecResult {
+        loop {
+            let result = self.run();
+            match result {
+                ExecResult::Load { address } => match memory.get(address as usize).copied() {
+                    None => return result,
+                    Some(data) => {
+                        self.state.load_data_8(data);
+                    }
+                },
+                ExecResult::Load16 { address } => {
+                    let address = address as usize;
+                    if address + 1 >= memory.len() {
+                        return result;
+                    } else {
+                        self.state
+                            .load_data_16([memory[address], memory[address + 1]]);
+                    }
+                }
+                ExecResult::Store { address, data } => {
+                    let address = address as usize;
+                    if address >= memory.len() {
+                        memory[address] = data;
+                    } else {
+                        return result;
+                    }
+                }
+                ExecResult::Store16 { address, data } => {
+                    let address = address as usize;
+                    if address + 1 >= memory.len() {
+                        memory[address] = data[0];
+                        memory[address + 1] = data[1];
+                    } else {
+                        return result;
+                    }
+                }
+                _ => return result,
+            }
+        }
+    }
+
+    /// Run the emulator with 64 kilobytes of memory.
+    pub fn run_with_full_memory(&mut self, memory: &mut [u8; 0x10000]) -> ExecResult {
+        loop {
+            let result = self.run();
+            match result {
+                ExecResult::Load { address } => self.state.load_data_8(memory[address as usize]),
+                ExecResult::Load16 { address } => {
+                    let address = address as usize;
+                    self.state
+                        .load_data_16([memory[address], memory[address + 1]]);
+                }
+                ExecResult::Store { address, data } => {
+                    memory[address as usize] = data;
+                }
+                ExecResult::Store16 { address, data } => {
+                    let address = address as usize;
+                    memory[address] = data[0];
+                    memory[address + 1] = data[1];
+                }
+                _ => return result,
+            }
+        }
+    }
 }
